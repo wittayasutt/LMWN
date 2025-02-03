2241efac805b2182e95f1bb7f586ce8adb623a11
Ratthapon Wittayasuttikul <ratthapon.w@freshket.co>
2025-02-03 17:57:10 +0700

    feat: add services

--- packages/api-gateway/routes/restaurants.ts
+++ packages/api-gateway/routes/restaurants.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getRestaurant } from '../services/restaurants';
+import { getRestaurant, getRestaurantShortMenu, getRestaurantFullMenu } from '../services/restaurants';
 
 const router = express.Router();
 
 router.get('/:id', getRestaurant);
+router.get('/:id/menus/:menuName', getRestaurantShortMenu);
+router.get('/:id/menus/:menuName/full', getRestaurantFullMenu);
 
 export default router;
--- packages/api-gateway/services/restaurants.ts
+++ packages/api-gateway/services/restaurants.ts
@@ -3,11 +3,51 @@ import { AxiosResponse } from 'axios';
 
 import $axios from '.';
 
-export const getRestaurant = async (req: Request, res: Response) => {
+// TODO: move to types
+type RestaurantParams = {
+	id: string;
+};
+
+type RestaurantMenuParams = {
+	id: string;
+	menuName: string;
+};
+
+export const getRestaurant = async (req: Request<RestaurantParams>, res: Response) => {
 	try {
 		const { id: restaurantId } = req.params;
 
 		const response: AxiosResponse = await $axios.get(`/restaurants/${restaurantId}.json`);
+
+		return res.status(response.status).json(response.data);
+	} catch (error: any) {
+		// TODO: add to middleware
+		const statusCode = error.response.status || 500;
+		return res.status(statusCode).json(error);
+	}
+};
+
+export const getRestaurantShortMenu = async (req: Request, res: Response) => {
+	try {
+		const { id: restaurantId, menuName } = req.params;
+		const url = `/restaurants/${restaurantId}/menus/${menuName}/short.json`;
+
+		const response: AxiosResponse = await $axios.get(url);
+
+		return res.status(response.status).json(response.data);
+	} catch (error: any) {
+		// TODO: add to middleware
+		const statusCode = error.response.status || 500;
+		return res.status(statusCode).json(error);
+	}
+};
+
+export const getRestaurantFullMenu = async (req: Request<RestaurantMenuParams>, res: Response) => {
+	try {
+		const { id: restaurantId, menuName } = req.params;
+		const url = `/restaurants/${restaurantId}/menus/${menuName}/full.json`;
+
+		const response: AxiosResponse = await $axios.get(url);
 
 		return res.status(response.status).json(response.data);
 	} catch (error: any) {
